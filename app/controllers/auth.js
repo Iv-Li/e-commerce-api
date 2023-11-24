96ee5b3128b669bf66ef32897bc14c73f1bac953
Ivanna Liuba <i.ivanna.liuba@gmail.com>
2023-11-24 14:44:18 +0100

    added register controller logic; hash password

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -1,5 +1,14 @@
-const register = (req, res) => {
-  res.send('register')
+const { StatusCodes } = require('http-status-codes')
+const User = require('../models/user')
+const { BadRequest } = require('../errors')
+const register = async (req, res) => {
+  const { email } = req.body
+  const isEmailExisted = await User.findOne({email})
+  if (isEmailExisted) {
+    throw new BadRequest(`Duplicate value: ${email} already exists`)
+  }
+  const user = await User.create(req.body)
+  res.status(StatusCodes.CREATED).json({ user, success: 'success'})
 }
 
 const login = (req, res) => {
--- app/models/user.js
+++ app/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcrypt')
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -30,5 +31,16 @@ const UserSchema = new mongoose.Schema({
     default: 'user'
   },
 })
+
+
+UserSchema.pre('save', async function() {
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
+
+UserSchema.methods.comparePassword = async function(comparedPassword) {
+  const isMatch = await bcrypt.compare(comparedPassword, this.password)
+  return isMatch
+}
 
 module.exports = mongoose.model('User', UserSchema)
